11e0ea8a721102850fc0743e30921f11f8e797b1
anassk01 <anaskabil01@gmail.com>
2026-01-14 23:02:10 +0100

    adding js logic

---
+++ projet1_HealthCare/script.js
@@ -0,0 +1,36 @@
+let mainForm = document.getElementById("main-form");
+let tableBody = document.getElementById("table-body");
+let id = 1;
+mainForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let nom = document.getElementById("nom");
+  let prenom = document.getElementById("prenom");
+  let email = document.getElementById("email");
+  let motif = document.getElementById("motif");
+  let phone = document.getElementById("phone");
+  let date = document.getElementById("date");
+
+  let demande = `
+  <td>${nom.value}</td>
+  <td>${prenom.value}</td>
+  <td>${email.value}</td>
+  <td>${phone.value}</td>
+  <td>${motif.value}</td>
+  <td>${date.value}</td>
+  <td><button>remove</button></td>
+  `;
+
+  const tableData = document.createElement("tr");
+  tableData.dataset.id = id;
+  tableData.innerHTML = demande.trim();
+  tableBody.appendChild(tableData);
+
+  let removeButton = tableData.querySelector("button");
+  id++;
+
+  removeButton.addEventListener("click", () => {
+    tableData.remove();
+  });
+});
+
+
