f4c96bde496a51f90ffba7ea20ec0c17799b80ad
anassk01 <anassk01@users.noreply.github.com>
2026-01-21 17:51:06 +0100

    adding pagination feature and editing css functions

--- projet1_HealthCare/script.js
+++ projet1_HealthCare/script.js
@@ -1,6 +1,14 @@
 let mainForm = document.getElementById("main-form");
 let tableBody = document.getElementById("table-body");
+const tableBatch = document.createElement("div");
+
+let precedentButton = document.getElementById("precedent");
+let suivantButton = document.getElementById("suivant");
+
 let id = 1;
+let batchId = 1;
+let isCreated = false;
+let currentPage = 1;
 mainForm.addEventListener("submit", (event) => {
   event.preventDefault();
   let nom = document.getElementById("nom");
@@ -18,14 +26,35 @@ mainForm.addEventListener("submit", (event) => {
   <td>${motif.value}</td>
   <td>${date.value}</td>
   <td><button>remove</button></td>
+
   `;
 
   const tableData = document.createElement("tr");
-  tableData.dataset.id = id;
+
+  //   tableData.dataset.id = id;
   tableData.innerHTML = demande.trim();
+
+  console.log("before", batchId);
   tableBody.appendChild(tableData);
 
   let removeButton = tableData.querySelector("button");
+  let idStage = Math.floor(id / 5);
+
+  tableData.dataset.id = batchId;
+
+  console.log("batchId", batchId, "idStage", idStage);
+  if (batchId === idStage) {
+    batchId++;
+    console.log("after", batchId);
+  }
+
+  tableBody.childNodes.forEach((el) => {
+    console.log("seo", batchId, parseInt(el.dataset.id));
+    if (parseInt(el.dataset.id) !== 1 && el.tagName === "TR") {
+      el.hidden = true;
+    }
+  });
+
   id++;
 
   removeButton.addEventListener("click", () => {
@@ -33,4 +62,28 @@ mainForm.addEventListener("submit", (event) => {
   });
 });
 
+precedentButton.addEventListener("click", () => {
+  currentPage--;
+  //   console.log("clciked");
+  tableBody.childNodes.forEach((el) => {
+    // console.log(el.dataset.id, batchId - 1);
+    if (parseInt(el.dataset.id) === currentPage) {
+      el.removeAttribute("hidden");
+    } else {
+      el.hidden = true;
+    }
+  });
+});
 
+suivantButton.addEventListener("click", () => {
+  currentPage++;
+
+  //   console.log("clciked");
+  tableBody.childNodes.forEach((el) => {
+    if (parseInt(el.dataset.id) === currentPage) {
+      el.removeAttribute("hidden");
+    } else {
+      el.hidden = true;
+    }
+  });
+});
